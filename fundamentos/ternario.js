83d6b4497778cd5c50a9956cd487c84300ffeff4
karmodaniel <daniel.carmo2008@gmail.com>
2020-03-03 20:13:08 -0400

    fundamentos(ternario)

---
+++ fundamentos/ternario.js
@@ -0,0 +1,10 @@
+//operador ternario
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
+
+const nota = 7.1
+const nota2 = 6.4
+
+console.log(resultado(nota))
+console.log(resultado(nota2))
+
+
