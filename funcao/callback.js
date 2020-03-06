696ca32949a2890a662892820f9ab8ea211ff1b6
karmodaniel <daniel.carmo2008@gmail.com>
2020-03-06 09:20:59 -0400

    funcao(callbacks)

---
+++ funcao/callback.js
@@ -0,0 +1,8 @@
+const fabricantes = ['Mercedes', 'Audi', 'BMW']
+//forEach aceita até três parametros
+//retorna os valores do array, os indices e o array todo
+fabricantes.forEach(function(valor, valor2, valor3){
+    console.log(valor, valor2, valor3)
+
+})
+
