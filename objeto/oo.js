5296fbaa063f02ec46d6401ccf29f028400bde29
karmodaniel <daniel.carmo2008@gmail.com>
2020-03-09 11:07:07 -0400

    Conceitos OO

---
+++ objeto/oo.js
@@ -0,0 +1,24 @@
+//Procedural
+processamento(valor1, valor2, valor3)
+
+//  OO  
+
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+
+obejeto.processamento() //Foco passou a ser o objeto
+//principios importantes:
+// 1. asbtração
+// 2. encapsulamento
+// 3. herença
+// 4. polimorfismo
+
+
+
+
