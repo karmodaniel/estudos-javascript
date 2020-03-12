55844ca9db6fd641655e8f6439d2f062a2b9733e
karmodaniel <daniel.carmo2008@gmail.com>
2020-03-12 17:17:42 -0400

    Objetos sem modificações

---
+++ objeto/evitandoModificacoes.js
@@ -0,0 +1,27 @@
+//Object.preventExtensions
+//evita adicionar novos atributos no objeto
+
+const produto = Object.preventExtensions({
+    nome: 'Qualquer', preco: '1.99', tag: 'promoção'
+})
+
+console.log(`Extensivel:`, Object.isExtensible(produto))
+
+produto.nome = 'Borracha'
+produto.descricao = 'Borracha escolar branca'
+delete produto.tag
+console.log(produto)
+
+//Object.seal
+//evita adicionar e deletar atributos do objeto
+
+const pessoa = {nome: 'Juliana', idade: 35}
+Object.seal(pessoa)
+console.log('Selado: ', Object.isSealed(pessoa))
+
+pessoa.sobrenome = 'Silva'
+delete pessoa.nome
+pessoa.idade = 29
+console.log(pessoa)
+
+//Object.freeze = selado + valores constantes, ou seja, nao é possivel modificar o objeto
