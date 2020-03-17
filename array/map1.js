463996ca68467dae01c7f0ba4b609e348e19dadb
karmodaniel <daniel.carmo2008@gmail.com>
2020-03-17 07:48:41 -0400

    update map1

--- array/map1.js
+++ array/map1.js
@@ -7,3 +7,10 @@ let result = nums.map(function(element) {
 })
 
 console.log(result)
+
+const soma10 = e => e + 10
+const triplo = e => e * 3
+const paraDinheiro = e => `R$ ${parseFloat(e).toFixed(2).replace('.',',')}`
+
+result = nums.map(soma10).map(triplo).map(paraDinheiro)
+console.log(result)
