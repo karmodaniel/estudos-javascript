962df6f323fa2b64f9edd311bd78f306cf2bc0f9
karmodaniel <daniel.carmo2008@gmail.com>
2020-03-24 01:57:19 -0400

    API simples com express parte 1

---
+++ node/projeto/src/servidor.js
@@ -0,0 +1,11 @@
+const port = 3003
+const express = require('express')
+const app = express()
+
+app.get('/produtos', (req, res, netx) => {
+        res.send({nome: 'Notebook', preco: '123.45'})
+})
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
+})
