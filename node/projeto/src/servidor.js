669f644cdba0ae393d2c0d41efdd80de9f80a873
karmodaniel <daniel.carmo2008@gmail.com>
2020-03-24 14:58:58 -0400

    api simples parte 2

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -1,9 +1,40 @@
 const port = 3003
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const bd = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({ extended: true}))
+
 
 app.get('/produtos', (req, res, netx) => {
-        res.send({nome: 'Notebook', preco: '123.45'})
+        res.send(bd.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, netx) => {
+    res.send(bd.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, netx) => {
+    const produto = bd.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+app.put('/produtos:id', (req, res, netx) => {
+    const produto = bd.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) //JSON
+})
+
+app.delete('/produtos:id', (req, res, netx) => {
+    const produto = bd.excluirProduto(req.params.id)
+    res.send(produto) //JSON
 })
 
 app.listen(port, () => {
