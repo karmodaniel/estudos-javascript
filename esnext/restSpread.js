0812f3cfd3d7b8a97098427d65bd9f7a19d32fa8
karmodaniel <daniel.carmo2008@gmail.com>
2020-03-25 04:07:28 -0400

    Operadores Rest e Spread

---
+++ esnext/restSpread.js
@@ -0,0 +1,14 @@
+//operado ... rest(juntar)/spread(espalhar)
+//usar rest com parâmetro de função
+
+
+//usar spread com objeto
+const funcionario = { nome: 'Maria', salario: 12348.99 }
+const clone = { ativo: true, ...funcionario}
+
+console.log(clone)
+
+//usar spread com array
+const grupoA = ['Daniel', 'Davi', 'Marilia']
+const grupoFinal = ['Francisco', ...grupoA, 'Ana']
+console.log(grupoFinal)
